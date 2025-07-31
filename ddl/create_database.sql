bf267158876dc37fec71f8483fc42bb8da6d5977
Kevin Rivera <kevinriveravergaray@gmail.com>
2025-07-30 21:45:33 -0500

    sucursales-empleados

--- ddl/create_database.sql
+++ ddl/create_database.sql
@@ -23,3 +23,7 @@ codigo VARCHAR(6) UNIQUE NOT NULL,
 nombre VARCHAR(155) UNIQUE NOT NULL,
 ubigeo_id INT 
 );
+
+-- empleados
+
+CREATE TABLE empleados (    id INT IDENTITY(1,1) PRIMARY KEY,    nombres VARCHAR(155) NOT NULL,    apellidos VARCHAR(155) NOT NULL,    numero_registro CHAR(6) UNIQUE NOT NULL,    cargo VARCHAR(100) NULL,    unidad VARCHAR(100) NOT NULL,    email VARCHAR(100) UNIQUE,    celular VARCHAR(20) UNIQUE);-- SucursalesCREATE TABLE sucursales (    id INT IDENTITY(1,1) PRIMARY KEY,    zona_id INT FOREIGN KEY REFERENCES zonas(id),    gerente_id INT FOREIGN KEY REFERENCES empleados(id),    codigo VARCHAR(6) UNIQUE NOT NULL,    nombre VARCHAR(100) UNIQUE NOT NULL,    direccion NVARCHAR(255) UNIQUE NOT NULL,    email VARCHAR(100) UNIQUE,    telefono VARCHAR(20) UNIQUE,    coordenada_gps GEOGRAPHY);EXEC sp_help sucursales;
