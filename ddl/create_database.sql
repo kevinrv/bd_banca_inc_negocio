fa25d5400de24ec7d9fc6dd624735067bc6e905d
Kevin Rivera <kevinriveravergaray@gmail.com>
2025-07-30 21:24:25 -0500

    create tipos indicador/zonas

--- ddl/create_database.sql
+++ ddl/create_database.sql
@@ -1,2 +1,25 @@
+-- Creaci+on base de datos
+CREATE DATABASE bd_banca_inc_negocio;
+--DROP DATABASE bd_banca_inc_negocio;
+USE bd_banca_inc_negocio;
+-- Creación de Tablas
 
-DROP DATABASE bd_banca_inc_negocio;
+--- Tipos de Indicador
+
+CREATE TABLE tipos_indicador (
+id INT IDENTITY(1,1) PRIMARY KEY,
+nombre VARCHAR(255) UNIQUE NOT NULL,
+descripcion VARCHAR(MAX) NULL
+);
+-- Para verficar los detalles de las tablas
+EXEC sp_help tipos_indicador;
+
+
+--Zonas 
+
+CREATE TABLE zonas(
+id INT IDENTITY(1,1) PRIMARY KEY,
+codigo VARCHAR(6) UNIQUE NOT NULL,
+nombre VARCHAR(155) UNIQUE NOT NULL,
+ubigeo_id INT 
+);
